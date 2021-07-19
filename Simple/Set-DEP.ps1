f9bdc2f206abf392aab1969dc472c722ef808d31
Darren Hollinrake <hollinrakedp@gmail.com>
2021-07-18 22:19:36 -0500

    Initial Commit: Set-DEP

---
+++ Simple/Set-DEP.ps1
@@ -0,0 +1,26 @@
+Function Set-DEP {
+    [cmdletbinding()]
+    Param(
+        [Parameter(
+            ValueFromPipelineByPropertyName,
+            ValueFromPipeline)]
+        [ValidateSet('AlwaysOff', 'AlwaysOn', 'OptIn', 'OptOut')]
+        [Alias("Value")]
+        [string[]]$Policy
+    )
+    $SetValue = switch ($Policy) {
+        AlwaysOff { 0 }
+        AlwaysOn { 1 }
+        OptIn { 2 }
+        OptOut { 3 }
+    }
+    $CurrentValue = (Get-CimInstance -ClassName Win32_OperatingSystem).DataExecutionPrevention_SupportPolicy
+    Write-Verbose "DEP Current Value = $CurrentValue"
+    
+    if ($SetValue -eq $CurrentValue) {
+        Write-Output "DEP is already set to $CurrentValue"
+        return
+    }
+    Write-Verbose "Setting DEP to $Policy"
+    BCDEDIT /set "{current}" nx $Policy
+}
