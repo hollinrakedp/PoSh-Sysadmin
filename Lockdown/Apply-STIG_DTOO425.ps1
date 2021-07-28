deeaf08f677e1ecea341039920249f0864e11556
Darren Hollinrake <hollinrakedp@gmail.com>
2021-07-27 20:31:20 -0500

    Removing Lockdown folder See DHHardenSystem module

--- Lockdown/Apply-STIG_DTOO425.ps1
+++
@@ -1,6 +0,0 @@
-Write-Output "Remediating STIG DTOO425"
-reg load "HKU\TEMP" "$env:SystemDrive\Users\Default\NTUSER.DAT"
-reg add "HKU\TEMP\SOFTWARE\Policies\Microsoft\Office\14.0\outlook\options\autoformat" /v pgrfafo_25_1 /t REG_DWORD /d 0 /f
-reg unload "HKU\TEMP"
-reg add "HKCU\SOFTWARE\Policies\Microsoft\Office\14.0\outlook\options\autoformat" /v pgrfafo_25_1 /t REG_DWORD /d 0 /f
-Write-Output "Remediation for STIG DTOO425 Completed"
