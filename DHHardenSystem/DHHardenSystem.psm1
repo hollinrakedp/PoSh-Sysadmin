d8bb88ce49561ae561fd1d6a200122c4a8665079
Darren Hollinrake <hollinrakedp@gmail.com>
2021-07-29 09:37:37 -0500

    Added tab completetion: Invoke-HardenSystem -Mitigation

--- DHHardenSystem/DHHardenSystem.psm1
+++ DHHardenSystem/DHHardenSystem.psm1
@@ -1,7 +1,8 @@
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+$Mitigation = @( Get-ChildItem -Path $PSScriptRoot\Mitigations\*.ps1 -ErrorAction SilentlyContinue )
 
-Foreach($script in @($Public + $Private)) {
+Foreach($script in @($Public + $Private + $Mitigation)) {
     Try
     {
         . $script.fullname
@@ -17,5 +18,10 @@ $EnvPath = $Env:Path -split ';'
 if (!($EnvPath -contains "$LGPOPath")) {
     $Env:Path += ";$LGPOPath"
 }
+
+#region Tab Completion
+$MitigationTabComplete = {Get-ChildItem -Path $(Join-Path "$($(Get-Module $($(Get-Command Invoke-HardenSystem).Source)).ModuleBase)" -ChildPath "Mitigations") -File | Select-Object -ExpandProperty BaseName}
+Register-ArgumentCompleter -CommandName Invoke-HardenSystem -ParameterName Mitigation -ScriptBlock $MitigationTabComplete
+#endregion Tab Completion
 
 Export-ModuleMember -Function $Public.Basename
