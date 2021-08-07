3a7ad38bfbc400bc586c3736ddd20254d49f7028
Darren Hollinrake <hollinrakedp@gmail.com>
2021-08-06 19:32:58 -0500

    Updated parameter to be 'Path'

--- Simple/Set-FirewallLogACL.ps1
+++ Simple/Set-FirewallLogACL.ps1
@@ -1,20 +1,28 @@
 function Set-FirewallLogACL {
+    [CmdletBinding(ConfirmImpact = 'High', SupportsShouldProcess)]
     param (
         [Parameter(
             ValueFromPipelineByPropertyName,
             ValueFromPipeline)]
-        [Alias("Path","LogPath")]
-        [string]$Directory = "$env:SystemRoot\System32\LogFiles\Firewall"
+        [string]$Path = "$env:SystemRoot\System32\LogFiles\Firewall"
     )
 
-    If (!(Test-Path $Directory)) {
-        New-Item -ItemType Directory -Path $Directory
+    begin {
+        if (!(Test-Path $Path)) {
+            Write-Error "The specified Path does not exist." -ErrorAction Stop
+        }
+    }
+    
+    process {
+        if ($PSCmdlet.ShouldProcess("$Path")) {
+            $Acl = Get-Acl $Path
+            $Acl | Format-List
+            $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule('NT Service\MpsSvc', 'Modify', 'ContainerInherit,ObjectInherit', 'None', 'Allow')
+            $Acl.SetAccessRule($AccessRule)
+            $Acl | Set-Acl $Path
+        }
     }
 
-    $Acl = Get-Acl $Directory
-    $Acl | Format-List
-    $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule('NT Service\MpsSvc', 'Modify', 'ContainerInherit,ObjectInherit', 'None', 'Allow')
-    $Acl.SetAccessRule($AccessRule)
-    $Acl | Set-Acl $Directory
-    Get-Acl $Directory | Format-List
+    end {}
+
 }
