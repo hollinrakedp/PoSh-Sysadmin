4315c1a06b49fd224482f465644fd17df51d77bc
Darren Hollinrake <hollinrakedp@gmail.com>
2021-12-31 19:37:43 -0600

    Initial Commit: Install-MSI

---
+++ Simple/Install-MSI.ps1
@@ -0,0 +1,57 @@
+function Install-MSI {
+    <#
+    .SYNOPSIS
+    Install MSI files.
+
+    .DESCRIPTION
+
+    .NOTES   
+    Name       : Install-MSI
+    Author     : Darren Hollinrake
+    Version    : 0.1
+    DateCreated: 2021-12-29
+    DateUpdated: 
+
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$Path = ".\",
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [switch]$Quiet,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [switch]$NoRestart,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$LogPath = "C:\Tenp\Logs"
+    )
+
+    begin {
+        $Installers = Get-ChildItem "$Path\*" -Include *.msi -Recurse
+    }
+    
+    process {
+        foreach ($MSI in $Installers) {
+            $MsiArgs = @(
+                "/i"
+                "`"$($MSI.FullName)`""
+            )
+            $MsiArgs += switch ($PSBoundParameters) {
+                Quiet { "/qn" }
+                NoRestart { "/norestart" }
+                LogPath { "/l* `"$LogPath`""}
+            }
+
+            # Run msiexec to install the application
+            $Install = Start-Process -FilePath msiexec -ArgumentList $MsiArgs -PassThru -Wait
+            $Install.WaitForExit()
+            If (($Install.ExitCode -eq 0) -or ($Install.ExitCode -eq 3010)) {
+                Write-Output "Installed $MSI successfully."
+            }
+            else {
+                Write-Warning "Installation of $MSI failed with code: $($Install.ExitCode)"
+            }
+        }
+    }
+    
+    end {}
+}
