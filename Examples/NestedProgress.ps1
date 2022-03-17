3c9587ad60c22a5454ab0fea1a03c45f479ae2ec
Darren Hollinrake <hollinrakedp@gmail.com>
2022-03-17 10:22:14 -0500

    Add Example: Nested Progress Bars

--- Examples/NestedProgress.ps1
+++ Examples/NestedProgress.ps1
@@ -1,7 +1,7 @@
 $max = 5
 for ($i = 1; $i -le $max; $i++) {
     Write-progress -Activity "Counting I" -Status "`$i = $i" -percentcomplete ($i / $max * 100) -id 1
-    sleep 1
+    Start-Sleep 1
     for ($j = 1; $j -le $max; $j++) {
         Write-progress -Activity "Counting J" -Status "`$j = $j" -percentcomplete ($j / $max * 100) -ParentID 1
         Start-Sleep 1
