5cf6d9e6320d482cdc188ad6940d53b279b89bd7
Darren Hollinrake <hollinrakedp@gmail.com>
2022-05-07 16:13:41 -0500

    Rename Scan-UpdateOffline

--- Simple/Invoke-OfflineWindowsUpdateScan.ps1
+++ Simple/Invoke-OfflineWindowsUpdateScan.ps1
@@ -8,17 +8,20 @@ The script can be used to scan the local system for missing Microsoft patches.
 The script requires the latest WSUS Scan Offline CAB file (wsusscn2.cab). Download the latest version from here: http://go.microsoft.com/fwlink/p/?LinkID=74689
 
 .NOTES   
-Name       : Scan-UpdateOffline.ps1
+Name       : Invoke-OfflineWindowsUpdateScan.ps1
 Author     : Darren Hollinrake
-Version    : 0.1
+Version    : 0.2
 DateCreated: 2018-11-05
-DateUpdated: 
+DateUpdated: 2022-05-07
 
-.PARAMETER ScanFilePath
+.PARAMETER Path
 Specify the location of the wsusscn.cab file to be used for the scan. The default is "C:\temp\wsusscn2.cab"
 
+.LINK
+http://go.microsoft.com/fwlink/p/?LinkID=74689
+
 .EXAMPLE
-Clear-EventLog
+.\Invoke-OfflineWindowsUpdateScan.ps1 -Path "C:\temp\wsusscn2.cab"
 
 #>
 
@@ -27,34 +30,24 @@ Clear-EventLog
 
 Param(
     [Parameter()]
-    [string]$ScanFilePath = "C:\temp\wsusscn2.cab",
-    [Parameter()]
-    [switch]$scheduletask,
-    [Parameter()]
-    [switch]$removetask
+    [string]$Path = "C:\temp\wsusscn2.cab"
 )
 
-########################
-#      Variables       #
-########################
-#region Variables
+
 $UpdateSession = New-Object -ComObject Microsoft.Update.Session
-$UpdateServiceManager  = New-Object -ComObject Microsoft.Update.ServiceManager
-$UpdateService = $UpdateServiceManager.AddScanPackageService("Offline Sync Service", "$ScanFilePath", 1)
+$UpdateServiceManager = New-Object -ComObject Microsoft.Update.ServiceManager
+$UpdateService = $UpdateServiceManager.AddScanPackageService("Offline Sync Service", "$Path", 1)
 $UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
-#endregion Variables
 
 Write-Output "Searching for updates... `r`n"
 
 $UpdateSearcher.ServerSelection = 3 #ssOthers
-
 $UpdateSearcher.ServiceID = $UpdateService.ServiceID
 
-$SearchResult = $UpdateSearcher.Search("IsInstalled=0") # or "IsInstalled=0 and IsInstalled=1" to also list the installed updates as MBSA did
-
+$SearchResult = $UpdateSearcher.Search("IsInstalled=0") # Change to "IsInstalled=0 and IsInstalled=1" to list the needed and installed updates as the MBSA did.
 $Updates = $SearchResult.Updates
 
-if($Updates.Count -eq 0){
+if ($Updates.Count -eq 0) {
     Write-Output "There are no applicable updates."
     return $null
 }
@@ -62,7 +55,7 @@ if($Updates.Count -eq 0){
 Write-Output "List of applicable items on the machine when using wssuscan.cab: `r`n"
 
 $i = 1
-foreach($Update in $Updates){
+foreach ($Update in $Updates) {
     Write-Output "$($i)> $($Update.Title)"
     $i++
 }
