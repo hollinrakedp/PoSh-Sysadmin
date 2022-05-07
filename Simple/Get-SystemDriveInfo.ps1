8244db521c4b34030e92e2439a2602fd6bfd4f46
Darren Hollinrake <hollinrakedp@gmail.com>
2022-05-07 14:25:58 -0500

    Remove commented out line

--- Simple/Get-SystemDriveInfo.ps1
+++ Simple/Get-SystemDriveInfo.ps1
@@ -4,7 +4,7 @@ Function Get-SystemDriveInfo {
     begin {}
     process {
         $LogicalDisk = Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object SystemName, DeviceID, Size, FreeSpace, DriveType
-        #, @{Name = "Size (GB)"; Expression = { $_.Size / 1gb -as [int] } }, @{Name = "Used (GB)"; Expression = { ($_.Size - $_.FreeSpace) / 1gb -as [int] } }, @{Name = "Free (GB)"; Expression = { $_.FreeSpace / 1gb -as [int] } }, @{Name = "% Free"; Expression = { $_.FreeSpace / $_.Size * 100 -as [int] } }
+
         foreach ($Disk in $LogicalDisk) {
             $DriveType = switch ($Disk.DriveType) {
                 1 { "NoRootDirectory" }
