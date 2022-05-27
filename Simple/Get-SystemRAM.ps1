bbe582d7f8d3b48ca333522f3de5d66fa13fe832
Darren Hollinrake <hollinrakedp@gmail.com>
2022-05-27 15:44:14 -0500

    Add space to name output

--- Simple/Get-SystemRAM.ps1
+++ Simple/Get-SystemRAM.ps1
@@ -5,7 +5,7 @@ Function Get-SystemRAM {
     begin {}
     process {
         $RAM = Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object Capacity -Sum
-        $RAM | Select-Object @{Name = "RAM(GB)"; Expression = { [Math]::Round($RAM.Sum / 1GB) } }
+        $RAM | Select-Object @{Name = "RAM (GB)"; Expression = { [Math]::Round($RAM.Sum / 1GB) } }
     }
     end {}
 }
