3157ac01e8bcc16d443cc64bdab00a62ccc8849e
Darren Hollinrake <hollinrakedp@gmail.com>
2022-05-27 15:13:33 -0500

    Return only uptime

--- Simple/Get-SystemUptime.ps1
+++ Simple/Get-SystemUptime.ps1
@@ -1,12 +1,4 @@
 function Get-SystemUptime {
-    param (
-        [Parameter()]
-        [switch]$BootTime
-    )
-
     $LastBootUpTime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
-    switch ($BootTime) {
-        True { $LastBootUpTime }
-        False { (New-TimeSpan -Start $LastBootUpTime).ToString("dd' days 'hh' hours 'mm' minutes 'ss' seconds'") }
-    }
+    (New-TimeSpan -Start $LastBootUpTime).ToString("dd' days, 'hh' hours, 'mm' minutes, 'ss' seconds'")
 }
