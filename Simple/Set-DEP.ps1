366c40c4708de526ab1958a90cf4d4880e0c18aa
Darren Hollinrake <50473419+hollinrakedp@users.noreply.github.com>
2024-01-22 19:01:25 -0600

    Update Set-DEP

--- Simple/Set-DEP.ps1
+++ Simple/Set-DEP.ps1
@@ -1,12 +1,27 @@
-Function Set-DEP {
-    [cmdletbinding()]
+function Set-DEP {
+    <#
+    .SYNOPSIS
+    Sets the Data Execution Prevention (DEP) policy on the system.
+
+    .DESCRIPTION
+    This function allows you to set the DEP policy on a Windows system. DEP is a security feature that helps prevent damage from viruses and other security threats that attempt to execute code from system memory locations reserved for Windows and other authorized programs.
+
+    .NOTES   
+    Name       : Set-DEP
+    Author     : Darren Hollinrake
+    Version    : 1.0
+    DateCreated: 2021-07-18
+    DateUpdated: 2024-01-22
+
+    #>
+    [CmdletBinding(ConfirmImpact = 'High', SupportsShouldProcess)]
     Param(
         [Parameter(
             ValueFromPipelineByPropertyName,
             ValueFromPipeline)]
         [ValidateSet('AlwaysOff', 'AlwaysOn', 'OptIn', 'OptOut')]
         [Alias("Value")]
-        [string[]]$Policy
+        [string]$Policy
     )
     $SetValue = switch ($Policy) {
         AlwaysOff { 0 }
@@ -14,13 +29,17 @@ Function Set-DEP {
         OptIn { 2 }
         OptOut { 3 }
     }
-    $CurrentValue = (Get-CimInstance -ClassName Win32_OperatingSystem).DataExecutionPrevention_SupportPolicy
-    Write-Verbose "DEP Current Value = $CurrentValue"
-    
-    if ($SetValue -eq $CurrentValue) {
-        Write-Output "DEP is already set to $CurrentValue"
-        return
+
+    if ($PSCmdlet.ShouldProcess("localhost", "Set-DEP $Policy")) {
+        Write-Verbose "Setting DEP to $Policy"
+        $CurrentValue = (Get-CimInstance -ClassName Win32_OperatingSystem).DataExecutionPrevention_SupportPolicy
+        if ($SetValue -eq $CurrentValue) {
+            Write-Output "DEP is already set to $CurrentValue"
+        }
+        else {
+            BCDEDIT /set "{current}" nx $Policy
+            Write-Output "DEP has been set. Reboot the system for the change to apply."
+        }
+        
     }
-    Write-Verbose "Setting DEP to $Policy"
-    BCDEDIT /set "{current}" nx $Policy
 }
