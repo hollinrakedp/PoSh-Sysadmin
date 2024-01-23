9c6cca8d5719b1aa978d8c37219ff3917ded9d4e
Darren Hollinrake <50473419+hollinrakedp@users.noreply.github.com>
2024-01-22 22:44:09 -0600

    Update Get-WindowsLicenseStatus

--- Simple/Get-WindowsLicenseStatus.ps1
+++ Simple/Get-WindowsLicenseStatus.ps1
@@ -1,4 +1,31 @@
 function Get-WindowsLicenseStatus {
+    <#
+    .SYNOPSIS
+    Get the license status of the Windows operating system on a local or remote computer.
+
+    .DESCRIPTION
+    This function retrieves and interprets the license status of the Windows operating system.
+    It queries the SoftwareLicensingProduct class to obtain licensing information and converts
+    the numeric license status into human-readable strings.
+
+    .NOTES
+    Name        : Get-WindowsLicenseStatus
+    Author      : Darren Hollinrake
+    Version     : 1.1
+    DateCreated : 2022-05-07
+    DateUpdated : 
+
+    .PARAMETER ComputerName
+    Specifies the target computer. If not provided, the function assumes the local computer.
+
+    .OUTPUTS
+    System.String
+
+    .EXAMPLE
+    Get-WindowsLicenseStatus
+    Get the license status of the Windows operating system on the local computer.
+
+    #>
     param (
         [Parameter()]
         [string]$ComputerName
@@ -10,7 +37,8 @@ function Get-WindowsLicenseStatus {
         else {
             $Result = Invoke-Command -ComputerName $ComputerName { Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" | Where-Object { $_.PartialProductKey } | Select-Object -Property LicenseStatus }   
         }
-        $Value = switch ($Result.LicenseStatus) {
+
+        switch ($Result.LicenseStatus) {
             0 { "Unlicensed" }
             1 { "Licensed" }
             2 { "OOBGrace" }
@@ -19,6 +47,5 @@ function Get-WindowsLicenseStatus {
             5 { "Notification" }
             6 { "ExtendedGrace" }
         }
-        $Value
     }
 }
