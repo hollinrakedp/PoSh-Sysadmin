1bc3470670eeb430d53f9bce0a5050b25b4cac90
Darren Hollinrake <50473419+hollinrakedp@users.noreply.github.com>
2024-01-22 18:49:27 -0600

    Update Get-SystemUptime

--- Simple/Get-SystemUptime.ps1
+++ Simple/Get-SystemUptime.ps1
@@ -1,4 +1,35 @@
 function Get-SystemUptime {
+    <#
+    .SYNOPSIS
+    Retrieves the system uptime or last boot time.
+
+    .DESCRIPTION
+    Returns the current system uptime or last boot time in a human-readable format.
+
+    .NOTES
+    Name        : Get-SystemUptime
+    Author      : Darren Hollinrake
+    Version     : 1.0
+    DateCreated : 2021-08-03
+    DateUpdated : 2024-01-22
+
+    .EXAMPLE
+    Get-SystemUptime
+    Returns the elapsed time since the system last booted.
+
+    .EXAMPLE
+    Get-SystemUptime -BootTime
+    Returns the last boot time.
+
+    #>
+    param (
+        [Parameter()]
+        [switch]$BootTime
+    )
+
     $LastBootUpTime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
-    (New-TimeSpan -Start $LastBootUpTime).ToString("dd' days, 'hh' hours, 'mm' minutes, 'ss' seconds'")
+    switch ($BootTime) {
+        True { $LastBootUpTime }
+        False { (New-TimeSpan -Start $LastBootUpTime).ToString("dd' days 'hh' hours 'mm' minutes 'ss' seconds'") }
+    }
 }
